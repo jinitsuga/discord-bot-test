e19fbde703583a3910180490e18852deb97b9bf6
Agu <iba.agustin@gmail.com>
2022-01-11 23:28:36 -0300

    baby changes, learning maps and pulling data from messages

---
+++ index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+
+const client = new Discord.Client({
+  intents: ["GUILDS", "GUILD_MESSAGES"],
+}); // new client for bot
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+// testing message replies
+client.on("messageCreate", (ms) => {
+  if (ms.content == "ping") {
+    ms.reply("pong");
+  }
+});
+client.on("messageCreate", (msg) => {
+  const tagged = Array.from(msg.mentions.users.values());
+  //console.log(msg.author.username + "#" + msg.author.discriminator);
+  console.log(tagged[0].username + "#" + tagged[0].discriminator);
+});
+
+let usersList = [];
+
+client.login(process.env.CLIENT_TOKEN);
